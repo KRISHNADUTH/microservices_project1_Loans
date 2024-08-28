afe13509f18e10e1a9470c6064ebcfee09a6d71f
Krishnaduth <krishnaduth777@gmail.com>
2024-08-28 18:55:05 +0530

    loans microservice completed

---
+++ src/main/java/com/eazybytes/loans/entity/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.eazybytes.loans.entity;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private String createAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private String updatedBy;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private String updatedAt;
+
+}
