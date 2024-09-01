4d9e22e2360fecc4e727a64c8512e2e49a41bb68
Krishnaduth <krishnaduth777@gmail.com>
2024-09-01 08:20:58 +0530

    Added - @EnableJpaAuditing(auditorAwareRef = auditAwareImpl) to main application class

--- src/main/java/com/eazybytes/loans/LoansApplication.java
+++ src/main/java/com/eazybytes/loans/LoansApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class LoansApplication {
 
 	public static void main(String[] args) {
